1c79bce5a085b8e0832f6ec34053a38e5e25b6e3
michel <1024836+michel-lacle@users.noreply.github.com>
2020-05-11 19:16:37 -0400

    install code deploy agent

--- webserver_install_1.sh
+++ webserver_install_1.sh
@@ -5,7 +5,7 @@ sudo yum update -y
 sudo yum install ruby -y
 sudo yum install wget -y
 cd /home/ec2-user
-wget https://aws-codedeploy-eu-central-1.s3.amazonaws.com/latest/install
+wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
 chmod +x ./install
 sudo ./install auto
 sudo service codedeploy-agent status
