78378c6421a1112ded01297417dfcb4a46079881
michel <1024836+michel-lacle@users.noreply.github.com>
2020-05-11 20:48:28 -0400

    adding cloudwatch agent logs

--- webserver_install_1.sh
+++ webserver_install_1.sh
@@ -16,7 +16,7 @@ sudo service codedeploy-agent status
 wget https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py
 wget https://s3.amazonaws.com/aws-codedeploy-us-east-1/cloudwatch/codedeploy_logs.conf
 chmod +x ./awslogs-agent-setup.py
-sudo python awslogs-agent-setup.py -n -r REGION -c s3://aws-codedeploy-us-east-1/cloudwatch/awslogs.conf
+sudo python awslogs-agent-setup.py -n -r us-east-1 -c s3://aws-codedeploy-us-east-1/cloudwatch/awslogs.conf
 sudo mkdir -p /var/awslogs/etc/config
 sudo cp codedeploy_logs.conf /var/awslogs/etc/config/
 sudo service awslogs restart
