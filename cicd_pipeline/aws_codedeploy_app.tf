f65d7dd0d823fe8f78306bfc456df7a96e2001ea
michel <1024836+michel-lacle@users.noreply.github.com>
2020-05-12 08:21:25 -0400

    add deployment group

--- cicd_pipeline/aws_codedeploy_app.tf
+++ cicd_pipeline/aws_codedeploy_app.tf
@@ -1,3 +1,53 @@
 resource "aws_codedeploy_app" "this" {
   name = var.application_name
 }
+
+resource "aws_iam_role" "codedeploy" {
+  name = "codeploy-${var.application_name}"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codedeploy.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_codedeploy_deployment_group" "this" {
+  app_name              = aws_codedeploy_app.this.name
+  deployment_group_name = var.application_name
+  service_role_arn      = aws_iam_role.codedeploy.arn
+
+  ec2_tag_set {
+    ec2_tag_filter {
+      key   = "filterkey1"
+      type  = "KEY_AND_VALUE"
+      value = "filtervalue"
+    }
+
+    ec2_tag_filter {
+      key   = "filterkey2"
+      type  = "KEY_AND_VALUE"
+      value = "filtervalue"
+    }
+  }
+
+  auto_rollback_configuration {
+    enabled = true
+    events  = ["DEPLOYMENT_FAILURE"]
+  }
+
+  alarm_configuration {
+    alarms  = ["my-alarm-name"]
+    enabled = true
+  }
+}
