69b4e91db5acc7a69af9d7b40c4c5c43d731c546
michel <1024836+michel-lacle@users.noreply.github.com>
2020-05-12 09:18:22 -0400

    adding role permissions for codedeploy

--- cicd_pipeline/aws_codedeploy_app.tf
+++ cicd_pipeline/aws_codedeploy_app.tf
@@ -22,21 +22,81 @@ resource "aws_iam_role" "codedeploy" {
 EOF
 }
 
+data "aws_iam_policy_document" "codedeploy" {
+
+  // allows logging to cloudwatch
+  statement {
+    actions = [
+      "autoscaling:CompleteLifecycleAction",
+      "autoscaling:DeleteLifecycleHook",
+      "autoscaling:DescribeAutoScalingGroups",
+      "autoscaling:DescribeLifecycleHooks",
+      "autoscaling:PutLifecycleHook",
+      "autoscaling:RecordLifecycleActionHeartbeat",
+      "autoscaling:CreateAutoScalingGroup",
+      "autoscaling:UpdateAutoScalingGroup",
+      "autoscaling:EnableMetricsCollection",
+      "autoscaling:DescribeAutoScalingGroups",
+      "autoscaling:DescribePolicies",
+      "autoscaling:DescribeScheduledActions",
+      "autoscaling:DescribeNotificationConfigurations",
+      "autoscaling:DescribeLifecycleHooks",
+      "autoscaling:SuspendProcesses",
+      "autoscaling:ResumeProcesses",
+      "autoscaling:AttachLoadBalancers",
+      "autoscaling:PutScalingPolicy",
+      "autoscaling:PutScheduledUpdateGroupAction",
+      "autoscaling:PutNotificationConfiguration",
+      "autoscaling:PutLifecycleHook",
+      "autoscaling:DescribeScalingActivities",
+      "autoscaling:DeleteAutoScalingGroup",
+      "ec2:DescribeInstances",
+      "ec2:DescribeInstanceStatus",
+      "ec2:TerminateInstances",
+      "tag:GetResources",
+      "sns:Publish",
+      "cloudwatch:DescribeAlarms",
+      "cloudwatch:PutMetricAlarm",
+      "elasticloadbalancing:DescribeLoadBalancers",
+      "elasticloadbalancing:DescribeInstanceHealth",
+      "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
+      "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
+      "elasticloadbalancing:DescribeTargetGroups",
+      "elasticloadbalancing:DescribeTargetHealth",
+      "elasticloadbalancing:RegisterTargets",
+      "elasticloadbalancing:DeregisterTargets"
+    ]
+
+    effect = "Allow"
+
+    resources = [
+      "*"
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "codebuild" {
+  role = aws_iam_role.codedeploy.name
+
+  policy = data.aws_iam_policy_document.codedeploy.json
+}
+
 resource "aws_codedeploy_deployment_group" "this" {
-  app_name              = aws_codedeploy_app.this.name
+  app_name = aws_codedeploy_app.this.name
   deployment_group_name = var.application_name
-  service_role_arn      = aws_iam_role.codedeploy.arn
+  service_role_arn = aws_iam_role.codedeploy.arn
 
   ec2_tag_set {
     ec2_tag_filter {
-      key   = "Project"
-      type  = "KEY_AND_VALUE"
+      key = "Project"
+      type = "KEY_AND_VALUE"
       value = var.application_name
     }
   }
 
   auto_rollback_configuration {
     enabled = true
-    events  = ["DEPLOYMENT_FAILURE"]
+    events = [
+      "DEPLOYMENT_FAILURE"]
   }
 }
